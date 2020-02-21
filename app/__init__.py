57cd9f0af4f5426bca48ed6a80fabaa3750a2e6f
SimonAwiti <awitisimon23@gmail.com>
2020-02-21 08:51:23 +0300

    app stracture development

---
+++ app/__init__.py
@@ -0,0 +1,44 @@
+# app/__init__.py
+import os
+from flask import Flask, redirect, jsonify,render_template
+from flask_restful import Api
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from datetime import datetime, timedelta
+
+# local import
+#from app.database.connection import initializedb
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.url_map.strict_slashes = False
+    CORS(app)
+
+  
+    def configure_blueprints(app):
+        """ Configure blueprints . """
+        #from app.api.routes.routes import auth_blueprint
+
+        #app_blueprints = [ auth_blueprint ]
+
+        #for bp in app_blueprints:
+            #CORS(bp)
+            #app.register_blueprint(bp)
+
+    """register the blueprints"""
+    configure_blueprints(app) 
+ 
+
+ 
+    #initializedb()
+    app.config['JWT_SECRET_KEY'] = 'SECRET'
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=100) 
+    jwt = JWTManager(app)
+    app.secret_key = "secret key"
+
+
+
+    @app.route('/')
+    def root():
+        return redirect ('https://documenter.getpostman.com/view/5353857/SVSHtVmy?version=latest')
+    return app
