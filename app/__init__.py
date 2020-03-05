8a481235406e9dac688cf1ea040e3676ed335121
SimonAwiti <awitisimon23@gmail.com>
2020-03-05 19:38:08 +0300

    complete app setup

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,13 @@ from datetime import datetime, timedelta
 
 #local import
 from app.db.connections import initializedb
+from instance.config import app_config
+
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
     app.url_map.strict_slashes = False
     CORS(app)
 
